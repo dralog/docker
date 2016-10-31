031695df2e5154c29804bee5b8c60f55fa3978ba
Sven Roeseler <sven@dralog.net>
2016-10-31 16:52:21 +0100

    Changed default local port for SSH at GitLab image from 22 to 8022

--- gitlab/run.sh
+++ gitlab/run.sh
@@ -4,7 +4,7 @@ docker run --detach \
 	--hostname ${GITLAB_HOSTNAME:=`hostname -f`} \
 	--publish ${GITLAB_HTTP_PORT:=80}:80 \
 	--publish ${GITLAB_HTTPS_PORT:=443}:443 \
-	--publish ${GITLAB_SSH_PORT:=22}:22 \
+	--publish ${GITLAB_SSH_PORT:=8022}:22 \
 	--name ${GITLAB_IMAGE_NAME:=gitlab} \
 	--restart always \
 	--volume ${GITLAB_CONFIG_DIR:=/srv/gitlab/config}:/etc/gitlab:Z \

04e28fca7672786d6c365b2c69941ca65f990088
Sven Roeseler <sven@svero.de>
2016-11-01 10:50:09 -0400

    Old DataStax image files removed

--- ubuntu-dse/README.md
+++
@@ -1,3 +0,0 @@
-# DataStax Enterprise on Ubuntu
-
-This project creates a Docker image for DataStax Enterprise based on Ubuntu 16.04 LTS and OpenJDK 8.
--- ubuntu-dse/dependencies/download_dse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "" -o "$2" == "" ]; then
-	echo "Usage: $0 <dsa_email_address> <password>"
-	exit 1
-fi
-
-curl --user $1:$2 -L http://downloads.datastax.com/enterprise/dse.tar.gz

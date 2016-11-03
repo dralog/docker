82cdb0e7f8ce71c0ce46ca4d8bbec6b4ec10a3c4
Sven Roeseler <sven@svero.de>
2016-11-03 05:14:16 -0400

    Added a run script for Jenkins which links the container to the Nexus container

---
+++ jenkins/run-with-nexus.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run \
+	--detach \
+	--hostname ${JENKINS_HOSTNAME:=`hostname -f`} \
+	--restart always \
+	--name ${JENKINS_CONTAINER_NAME:=jenkins} \
+	--publish ${JENKINS_HTTP_PORT:=8080}:8080 \
+	--publish ${JENKINS_SLAVE_PORT:=50000}:50000 \
+	--volume ${JENKINS_HOME_DIR:=/srv/jenkins}:/var/jenkins_home:Z \
+	--link ${NEXUS_CONTAINER_NAME:=nexus} \
+	dralog/jenkins:latest
